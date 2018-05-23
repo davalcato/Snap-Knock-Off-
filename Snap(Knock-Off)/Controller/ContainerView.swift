3279249ab8a197ca0f86039feed900baa40ba2a0
davalcato <davalcato@yahoo.com>
2018-05-22 20:40:16 -0700

    Added the Controller and View files

---
+++ Snap(Knock-Off)/Controller/ContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  ContainerView.swift
+//  Snap(Knock-Off)
+//
+//  Created by Daval Cato on 5/22/18.
+//  Copyright © 2018 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class ContainerView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+   
+
+}
---
+++ Snap(Knock-Off)/Controller/RightView.swift
@@ -0,0 +1,21 @@
+//
+//  RightView.swift
+//  Snap(Knock-Off)
+//
+//  Created by Daval Cato on 5/22/18.
+//  Copyright © 2018 Daval Cato. All rights reserved.
+//
+
+import UIKit
+
+class RightView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
